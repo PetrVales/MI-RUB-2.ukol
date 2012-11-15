428b65f8f8618080a5eb0db552bafe8f2395342a
Bc. Petr Valeš <valespe3@fit.cvut.cz>
2012-11-15 20:01:59 +0100

    modification of everything :D

--- Triangles.rb
+++ Triangles.rb
@@ -1,3 +1,5 @@
+include Math
+
 class Triangle
 	def initialize(a, b, c)
 		@a = a
@@ -18,50 +20,150 @@ class Triangle
 	end
 
 	def contains?(point)
-		b1 = sign(point, @a, @b) < 0
-		b2 = sign(point, @b, @c) < 0
-		b3 = sign(point, @c, @a) < 0
-		(b1 == b2) && (b2 == b3)
+		if ((@a == point) || (@b == point) || (@c == point))
+			return true
+		end
+		if sameSide(point, a, b, c) and sameSide(point, b, a, c) and sameSide(point, c, a, b) then
+			return true
+    	else
+    		return false
+    	end
 	end
 
 	def area 
 		(@a.x * (@b.y - @c.y) + @b.x * (@c.y - @a.y) + @c.x * (@a.y - @b.y)).abs / 2.0
 	end
 
-	def unionArea(triangle)
-		area = 0
+	def union(triangle)
 		if (contains?(triangle.a) && contains?(triangle.b) && contains?(triangle.c))
-			area = area()
-		elsif (triangle.contains?(@a) && triangle.contains?(@b) && triangle.contains?(@c))
-			area = triangle.area
-		elsif (isOneCommonVertex(triangle))
-			area = area() + triangle.area
-		elsif (isOneCommonEdge(triangle))
-			area = area() + triangle.area
-		# else
-		# 	area = area() + triangle.area - intersectArea(triangle)
+			Polygon.new([a, b, c])
+		elsif (triangle.contains?(a) && triangle.contains?(b) && triangle.contains?(c))
+			Polygon.new([triangle.a, triangle.b, triangle.c])
+		else
+			polygon = []
+			buildPolygon(polygon, triangle.a)
+			buildPolygon(polygon, triangle.b)
+			buildPolygon(polygon, triangle.c)
+			addLineIntersects(polygon, triangle)
+			# puts polygon.to_s
+			if polygon.length > 0 then
+				CompositePolygon.new(self, triangle, Polygon.new(polygon))
+			else
+				raise "no intersect area"
+			end
 		end
-			
-		area
 	end
 
 	private
-	def sign(p1, p2, p3)
-		(p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
+	def sameSide(testedPoint, knownPoint, i, j)
+		cp1 = crossProduct(j - i, testedPoint - i)
+	    cp2 = crossProduct(j - i, knownPoint - i)
+	    if dotProduct(cp1, cp2) >= 0 then 
+	    	return true
+	    else 
+	    	return false
+	    end
 	end
-
-	def isOneCommonVertex(triangle)
-		 ((!(contains?(triangle.a) && contains?(triangle.b)) && (triangle.c == @a || triangle.c == @b || triangle.c == @c)) ||
-					(!(contains?(triangle.b) && contains?(triangle.c)) && (triangle.a == @a || triangle.a == @b || triangle.a == @b)) ||
-					(!(contains?(triangle.c) && contains?(triangle.a)) && (triangle.b == @a || triangle.b == @b || triangle.b == @c)))
+	def crossProduct(i, j)
+		i.x * j.y - j.x * i.y
 	end
-
-	def isOneCommonEdge(triangle)
-
+	def dotProduct(i, j)
+		i * j
 	end
-
+	def buildPolygon(polygon, p)
+		if (contains?(p))
+			polygon.push(p)
+		end
+	end
+	def addLineIntersects(polygon, triangle)
+		addEdgeIntersection(polygon, triangle, [a, b], [triangle.a, triangle.b])
+		addEdgeIntersection(polygon, triangle, [a, b], [triangle.a, triangle.c])
+		addEdgeIntersection(polygon, triangle, [a, b], [triangle.c, triangle.b])
+		addEdgeIntersection(polygon, triangle, [a, c], [triangle.a, triangle.b])
+		addEdgeIntersection(polygon, triangle, [a, c], [triangle.a, triangle.c])
+		addEdgeIntersection(polygon, triangle, [a, c], [triangle.c, triangle.b])
+		addEdgeIntersection(polygon, triangle, [c, b], [triangle.a, triangle.b])
+		addEdgeIntersection(polygon, triangle, [c, b], [triangle.a, triangle.c])
+		addEdgeIntersection(polygon, triangle, [c, b], [triangle.c, triangle.b])
+	end
+	def addEdgeIntersection(polygon, triangle, x, y)
+		p = linesIntersect(x, y)
+		if (p != nil && contains?(p) && triangle.contains?(p))
+			polygon.push(p)
+		end
+	end
+	def linesIntersect(x, y)
+		line1 = Line.new(x[0], x[1])
+		line2 = Line.new(y[0], y[1])
+		line1.intersect(line2)
+	end
 end 
 
+class Polygon
+
+	def initialize(vertices)
+		center = centerPoint(vertices)
+		@vertices = vertices.sort {|a, b| 
+			Math.atan2(a.y - center.y, a.x - center.x) <=> Math.atan2(b.y - center.y, b.x - center.x)
+		}
+	end
+
+	def vertices
+		@vertices
+	end
+
+	def area
+		sum = 0
+		for i in 0..(@vertices.length - 1)
+			sum += @vertices[i].x * @vertices[(i + 1) % @vertices.length].y - @vertices[(i + 1) % @vertices.length].x * @vertices[i].y
+		end
+		(sum / 2).round(5).abs
+	end
+
+	def ==(polygon)
+		if @vertices.length != polygon.vertices.length
+			return false
+		end
+		for i in 0..(polygon.vertices.length - 1)
+			if @vertices.index(polygon.vertices[i]) == nil
+				return false
+			end
+		end
+		true
+	end
+
+	private
+	def centerPoint(vertices)
+		sumX = 0.0
+		sumY = 0.0
+		vertices.each {|x| sumX += x.x; sumY += x.y }
+		Point.new(sumX / vertices.length, sumY / vertices.length)
+	end
+end
+
+class CompositePolygon < Polygon
+
+	def initialize(a, b, p)
+		@a = a
+		@b = b
+		@p = p
+	end
+
+	def area
+		# puts @a.area.to_s + " + " + @b.area.to_s + " - " + @p.area.to_s
+		@a.area + @b.area - @p.area
+	end
+
+
+	def ==(polygon)
+		if (polygon.is_a? CompositePolygon)
+			@a == polygon.a && @b == polygon.b && @p == polygon.p
+		else
+			false
+		end
+	end
+
+end
 
 class Point
 	def initialize(x, y)
@@ -81,7 +183,67 @@ class Point
 		"[" + @x.to_s + ", " + @y.to_s + "]"
 	end
 
-	def ==(triangle)
-		@x == triangle.x && @y == triangle.y
+	def ==(point)
+		if point == nil 
+			false
+		else
+			@x == point.x && @y == point.y
+		end
+	end
+
+	def -(point)
+		Point.new(@x - point.x, @y - point.y)
+	end
+end
+
+class Line
+	def initialize(x, y)
+		@x = x
+		@y = y
+	end
+
+	def contains(p)
+		slope = ((@x.y - @y.y).to_f / (@x.x - @y.x))
+		slopeXP = ((@x.y - p.y).to_f / (@x.x - p.x))
+		slopePY = ((p.y - @y.y).to_f / (p.x - @y.x))
+		# puts slope.to_s + " ; " + slopeXP.to_s + " ; " + slopePY.to_s + "              " + @x.to_s + " ; " + @y.to_s + " ; " + p.to_s
+		(slope == slopeXP) && (slope == slopePY) && (p.x <= @x.x || p.x <= @y.x) && (p.y <= @x.y || p.y <= @y.y)
+	end
+
+	def intersect(line)
+		x1 = @x.x
+		y1 = @x.y
+		x2 = @y.x
+		y2 = @y.y
+		x3 = line.x.x
+		y3 = line.x.y
+		x4 = line.y.x
+		y4 = line.y.y
+		if (((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)) == 0)
+			return nil
+		else
+			x = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)).to_f / ((x1 -x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+			if (x.zero?)
+				x = 0.0
+			end
+			y = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)).to_f / ((x1 -x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+			if (y.zero?)
+				y = 0.0
+			end
+			p = Point.new(x, y)
+			if (!p.x.nan? && !p.y.nan? && p.x.finite? && p.y.finite? && contains(p) && line.contains(p))
+				return p
+			else
+				return nil
+			end
+		end
+	end
+
+	def x
+		@x
+	end
+
+	def y
+		@y
 	end
 end
--- TrianglesTest.rb
+++ TrianglesTest.rb
@@ -3,50 +3,93 @@ require './Triangles.rb'
 describe Triangle do
 
 	it "creats triangle from three points" do
-		a = Point.new(0,0)
-		b = Point.new(1,0)
-		c = Point.new(0,1)
-    	triangle = Triangle.new(a, b, c)
-    	triangle.a.should eq(a)
-    	triangle.b.should eq(b)
-    	triangle.c.should eq(c)
-  	end
+		a = Point.new(0, 0)
+		b = Point.new(1, 0)
+		c = Point.new(0, 1)
+    triangle = Triangle.new(a, b, c)
+    triangle.a.should eq(a)
+    triangle.b.should eq(b)
+    triangle.c.should eq(c)
+  end
 
-  	it "detemines if contains a point" do
-		a = Point.new(0,0)
+  it "detemines if contains a point" do
+
+		a = Point.new(0, 0)
+		b = Point.new(3, 0)
+		c = Point.new(0, 3)
+    triangle = Triangle.new(a, b, c)
+    triangle.contains?(Point.new(1, 1)).should eq(true)
+    triangle.contains?(Point.new(0, 0)).should eq(true)
+    triangle.contains?(Point.new(0.0, 0.0)).should eq(true)
+    triangle.contains?(Point.new(3, 3)).should eq(false)
+    Triangle.new(Point.new(10, 5), Point.new(20, -5), Point.new(0, -5)).contains?(Point.new(15.0, 0.0)).should eq(true)
+    Triangle.new(Point.new(10, 5), Point.new(20, -5), Point.new(0, -5)).contains?(Point.new(5.0, 0.0)).should eq(true)
+    Triangle.new(Point.new(10, 5), Point.new(20, -5), Point.new(0, -5)).contains?(Point.new(12.5, 7.5)).should eq(false)
+    Triangle.new(Point.new(0, 0), Point.new(20, 0), Point.new(0, 20)).contains?(Point.new(0, -5)).should eq(false)
+    Triangle.new(Point.new(0, 0), Point.new(10, 0), Point.new(0, 10)).contains?(Point.new(1, 1)).should eq(true)
+    Triangle.new(Point.new(0, 0), Point.new(10, 0), Point.new(0, 10)).contains?(Point.new(9, 0)).should eq(true)
+    Triangle.new(Point.new(0, 0), Point.new(10, 0), Point.new(0, 10)).contains?(Point.new(0, 9)).should eq(true)
+    Triangle.new(Point.new(0, 0), Point.new(10, 0), Point.new(0, 10)).contains?(Point.new(0.0, 1.0)).should eq(true)
+    Triangle.new(Point.new(1, 1), Point.new(9, 1), Point.new(1, 9)).contains?(Point.new(0.0, 1.0)).should eq(false)
+  end
+
+  it "computes its area" do
+  	a = Point.new(0,0)
 		b = Point.new(3,0)
 		c = Point.new(0,3)
-    	triangle = Triangle.new(a, b, c)
-    	triangle.contains?(Point.new(1,1)).should eq(true)
-    	triangle.contains?(Point.new(0,0)).should eq(true)
-    	triangle.contains?(Point.new(3,3)).should eq(false)
-  	end
+    triangle = Triangle.new(a, b, c)
+    triangle.area.should eq(4.5)
+  end
 
-  	it "computes its area" do
-  		a = Point.new(0,0)
-		b = Point.new(3,0)
-		c = Point.new(0,3)
-    	triangle = Triangle.new(a, b, c)
-    	triangle.area.should eq(4.5)
-  	end
+  it "creats polygon when unioned with a triangle - a triangle inside another triangle" do
+    biggerTriangle = Triangle.new(Point.new(0, 0), Point.new(10, 0), Point.new(0, 10))
+    smallerTriangle = Triangle.new(Point.new(1, 1), Point.new(9, 1), Point.new(1, 9))
+    biggerTriangle.union(smallerTriangle).area.should eq(50 + 32 - 32)
+    smallerTriangle.union(biggerTriangle).area.should eq(50.0000)
+  end
 
-  	it "computes union area of two triangles - if bigger triangle contains smaller triangle, union area equals the area of the bigger triangle" do
-    	biggerTriangle = Triangle.new(Point.new(0,0), Point.new(10,0), Point.new(0,10))
-    	smallerTriangle = Triangle.new(Point.new(1,1), Point.new(9,0), Point.new(0,9))
-    	biggerTriangle.unionArea(smallerTriangle).should eq(biggerTriangle.area)
-  	end
 
-  	it "computes union area of two triangles - area of two wertex connected triangles is sum of their areas" do
-    	firstTriangle = Triangle.new(Point.new(9,9), Point.new(9,0), Point.new(0,9))
-    	secondTriangle = Triangle.new(Point.new(0,0), Point.new(9,0), Point.new(0,9))
-    	firstTriangle.unionArea(secondTriangle).should eq(81)
-  	end
+  it "creats polygon when unioned with a triangle - star polygon" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(2, 0), Point.new(0, 2))
+    secondTriangle = Triangle.new(Point.new(2, 0), Point.new(2, 2), Point.new(0, 4))
+    firstTriangle.union(secondTriangle).area.should eq(4.0000)
+  end
 
-  	it "computes union area of two triangles - area of two edge connected triangles is sum of their areas" do
-    	firstTriangle = Triangle.new(Point.new(9,9), Point.new(9,0), Point.new(0,9))
-    	secondTriangle = Triangle.new(Point.new(0,0), Point.new(9,0), Point.new(0,9))
-    	firstTriangle.unionArea(secondTriangle).should eq(81)
-  	end
+  it "creats polygon when unioned with a triangle - one point in" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(20, 0), Point.new(0, 20))
+    secondTriangle = Triangle.new(Point.new(10, 5), Point.new(20, -5), Point.new(0, -5))
+    firstTriangle.union(secondTriangle).area.should eq(200 + 100 - 25)
+  end
+
+  it "creats polygon when unioned with a triangle - two points in" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(25, 0), Point.new(0, 25))
+    secondTriangle = Triangle.new(Point.new(5, 5), Point.new(15, 5), Point.new(10, -5))
+    firstTriangle.union(secondTriangle).area.should eq(312.5 + 50 - 37.5)
+  end
+
+  it "creats polygon when unioned with a triangle - point on edge" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(20, 0), Point.new(0, 20))
+    secondTriangle = Triangle.new(Point.new(10, 0), Point.new(15, -5), Point.new(5, -5))
+    firstTriangle.union(secondTriangle).area.should eq(200 + 25 - 0)
+  end
+
+  it "creats polygon when unioned with a triangle - edges intersects" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(20, 0), Point.new(0, 20))
+    secondTriangle = Triangle.new(Point.new(10, -5), Point.new(5, 0), Point.new(15, 0))
+    firstTriangle.union(secondTriangle).area.should eq(200 + 25 - 0)
+  end
+
+  it "creats polygon when unioned with a triangle - shared vertex" do
+    firstTriangle = Triangle.new(Point.new(0, 0), Point.new(20, 0), Point.new(0, 20))
+    secondTriangle = Triangle.new(Point.new(0, 0), Point.new(-20, 0), Point.new(0, -20))
+    firstTriangle.union(secondTriangle).area.should eq(200 + 200 - 0)
+  end
+
+  it "creats polygon when unioned with a triangle - no intersect" do
+    firstTriangle = Triangle.new(Point.new(1, 1), Point.new(20, 1), Point.new(1, 20))
+    secondTriangle = Triangle.new(Point.new(-1, -1), Point.new(-20, -1), Point.new(-1, -20))
+    expect { firstTriangle.union(secondTriangle) }.to raise_error
+  end
 
 end
 
@@ -58,13 +101,73 @@ describe Point do
 		point = Point.new(x, y)
 		point.x.should eq(x)
 		point.y.should eq(x)
-  	end
+  end
 
-  	it "equals to by value" do
+  it "equals to by value" do
 		point1 = Point.new(0, 0)
 		point2 = Point.new(0, 0)
 		(point1 == point2).should eq(true)
-  	end
+    (point1 == nil).should eq(false)
+  end
 
 end
+
+describe Line do
+  it "creates line defined by two points" do
+    point1 = Point.new(0, 0)
+    point2 = Point.new(4, 4)
+    line = Line.new(point1, point2)
+    line.contains(Point.new(1, 1)).should eq(true)
+    line.contains(Point.new(1, 2)).should eq(false)
+    line.contains(Point.new(5, 5)).should eq(false)
+    line = Line.new(Point.new(10, 5),Point.new(0, -5))
+    line.contains(Point.new(12.5, 7.5)).should eq(false)
+    Line.new(Point.new(0, 0), Point.new(0, 10)).contains(Point.new(0, 9)).should eq(true)
+    Line.new(Point.new(0, 0), Point.new(0, 10)).contains(Point.new(0, 11)).should eq(false)
+    Line.new(Point.new(0, 0), Point.new(20, 0)).contains(Point.new(0, -5)).should eq(false)
+    Line.new(Point.new(0, 0), Point.new(0, 20)).contains(Point.new(0, -5)).should eq(false)
+    Line.new(Point.new(0, 20), Point.new(20, 0)).contains(Point.new(0, -5)).should eq(false)
+  end
+
+  it "intersect with another line in a point" do
+    line1 = Line.new(Point.new(0, 0), Point.new(1, 1))
+    line2 = Line.new(Point.new(0, 1), Point.new(1, 0))
+    line1.intersect(line2).should eq(Point.new(0.5, 0.5))
+    line2.intersect(line2).should eq(nil)
+  end
+end
+
+# describe Polygon do
+
+#   it "creats 2d polygon from array of points" do
+#     point1 = Point.new(0, 0)
+#     point2 = Point.new(1, 0)
+#     point3 = Point.new(1, 1)
+#     point4 = Point.new(0, 1)
+#     polygon = Polygon.new([point1, point2, point3, point4])
+#     polygon.vertices.should eq([point1, point2, point3, point4])
+#   end
+
+#   it "computes its area" do
+#     point1 = Point.new(0, 0)
+#     point2 = Point.new(10, 0)
+#     point3 = Point.new(10, 10)
+#     point4 = Point.new(0, 10)
+#     polygon = Polygon.new([point1, point2, point3, point4])
+#     polygon.area.should eq(100)
+#   end
+
+#   it "equals to polygon with same vertices" do
+#     first = Polygon.new([Point.new(0, 0), Point.new(10, 0), Point.new(10, 10), Point.new(0, 10)])
+#     second = Polygon.new([Point.new(0, 0), Point.new(10, 0), Point.new(10, 10), Point.new(0, 10)])
+#     (first == second).should eq(true)
+#   end
+
+#   it "should not equal to polygon with different vertices" do
+#     first = Polygon.new([Point.new(1, 0), Point.new(10, 0), Point.new(10, 10), Point.new(0, 10)])
+#     second = Polygon.new([Point.new(0, 0), Point.new(10, 0), Point.new(10, 10), Point.new(0, 10)])
+#     (first == second).should eq(false)
+#   end
+
+# end
 
